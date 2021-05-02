66adefd15e51b5a9c196084cbe04195331662377
Jesus Lara <jelara99@gmail.com>
2021-05-02 01:07:44 +0000

    farmulario de inicio de sesion dentro del modal

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import logo from "../../img/starwars.png";
 import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 import ModalTitle from "react-bootstrap/ModalTitle";
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
@@ -10,13 +11,23 @@ export const Navbar = () => {
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
 	// const [vista, setVista] = useState(true);
 
 	const entrar = dato => {
 		actions.cambio(dato);
 		handleClose();
+		setEmail("");
+		setPassword("");
 	};
+	function validateForm() {
+		return email.length > 0 && password.length > 0;
+	}
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
 
 	return (
 		<nav className="navbar">
@@ -32,15 +43,40 @@ export const Navbar = () => {
 					</Button>
 
 					<Modal className="modalNav" show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-						<Modal.Header closeButton className="part">
+						<Modal.Header className="part">
 							<Modal.Title>Bienvenido!</Modal.Title>
 						</Modal.Header>
-						<Modal.Body className="part" />
+						<Modal.Body className="part">
+							<Form className="mt-3">
+								<Form.Group size="lg" controlId="email">
+									<Form.Label>Email</Form.Label>
+									<Form.Control
+										autoFocus
+										type="email"
+										value={email}
+										onChange={e => setEmail(e.target.value)}
+									/>
+								</Form.Group>
+								<Form.Group size="lg" controlId="password">
+									<Form.Label>Password</Form.Label>
+									<Form.Control
+										type="password"
+										value={password}
+										onChange={e => setPassword(e.target.value)}
+									/>
+								</Form.Group>
+							</Form>
+						</Modal.Body>
 						<Modal.Footer className="part">
 							<Button variant="btn-warning" className="btn-warning" onClick={handleClose}>
-								Close
+								Cerrar
 							</Button>
-							<Button variant="btn-warning" className="btn-warning" onClick={() => entrar(false)}>
+							<Button
+								variant="btn-warning"
+								className="btn-warning"
+								type="submit"
+								disabled={!validateForm()}
+								onClick={() => entrar(false)}>
 								Entrar
 							</Button>
 						</Modal.Footer>
